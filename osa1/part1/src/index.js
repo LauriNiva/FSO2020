f257c10451ed28de59163af25f91a4adb5219c73
Lauri Niva <niva.lauri@gmail.com>
2021-03-05 09:08:54 +0200

    Relearning (1.1)

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,27 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) =>{
+
+const Hello = (props) => {
   return(
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
-
-
   )
-
 }
 
 const App = () => {
-  const nimi = "Pekka"
-  const ika = 20
+  const nimi = "Pekka";
+  const ika = 10;
 
-  return(
+  return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={10+16} />
-      <Hello name={nimi} age={ika} />
-      <Hello age="2" />
+      <Hello name="Maya" age={26 + 10 }/>
+      <Hello name={nimi} age={ika}/>
     </div>
   )
 }
