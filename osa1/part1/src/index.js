f29724c1cb1e8da185310e73b04540f1f39d8254
Lauri Niva <niva.lauri@gmail.com>
2021-03-06 11:09:58 +0200

    Komponenttien tila harjoitusta

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Hello = (props) => {
-  return(
+const Hello = ({name, age}) => {
+
+  const bornYear = () => new Date().getFullYear() - age;
+  
+  return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born {bornYear()}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const nimi = "Pekka";
@@ -17,10 +25,10 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10 }/>
-      <Hello name={nimi} age={ika}/>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
