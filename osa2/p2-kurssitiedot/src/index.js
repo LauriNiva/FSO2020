31cccf6d04abf1c12a5c2cdc42ffa7990f46aa26
Lauri Niva <niva.lauri@gmail.com>
2021-03-07 19:48:06 +0200

    p2-kt: changed Total to use reduce

--- osa2/p2-kurssitiedot/src/index.js
+++ osa2/p2-kurssitiedot/src/index.js
@@ -6,10 +6,10 @@ const Header = (props) => {
   return <h1>{props.course.name}</h1>
 }
 
-const Content = (props) => {
+const Content = ({course}) => {
 
-  const content = props.course.parts.map(e => <Part part={e} />);
-  console.log(content);
+  const content = course.parts.map(part => <Part key={part.id} part={part} />);
+
   return (
     <div>
       {content}
@@ -25,42 +25,51 @@ const Part = (props) => {
   )
 }
 
-const Total = (props) => {
-  let totalNumber = 0;
-  props.course.parts.forEach(element => {
-    totalNumber += element.exercises
-  });
+const Total = ({course}) => {
+  let totalNumber = course.parts.reduce((accumulator, current) => {
+    return accumulator + current.exercises;
+  },0);
 
-  return <p>Number of exercises {totalNumber}</p>;
+  return <p>Total number of exercises {totalNumber}</p>;
 }
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
 
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-
+      <Course course={course} />
     </div>
   )
 }
