2cd69e3db52a044a3e57eabbdf5977bf52370d4f
Lauri Niva <niva.lauri@gmail.com>
2021-03-08 10:03:06 +0200

    p2-puhelinluettelo: 2.10 done: refactored

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,26 +1,73 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState("");
+    const [nameFilter, setNameFilter] = useState("");
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+
+    const handleNameChange = (e) => {
+        setNewName(e.target.value);
+    }
+
+    const handleNumberChange = (e) => {
+        setNewNumber(e.target.value);
+    }
+
+    const handleFilterChange = (e) => {
+        setNameFilter(e.target.value);
+    }
+
+    const addName = (e) => {
+        e.preventDefault();
+
+        if (persons.find(person => person.name === newName)) {
+            alert(`${newName} is already in the phonebook`);
+            setNewName("");
+            setNewNumber("");
+            return;
+        }
+
+        const newObject = {
+            name: newName,
+            number: newNumber
+        };
+        setPersons(persons.concat(newObject));
+
+        setNewName("");
+        setNewNumber("");
+    }
+
+    const personsToShow = persons.filter(person =>
+        person.name.toLowerCase().includes(nameFilter.toLowerCase())
+    );
+
+
+    return (
         <div>
-          name: <input />
+            <h1>Phonebook</h1>
+            <Filter value={nameFilter} onChangeHandler={handleFilterChange} />
+
+            <h2>Add a new</h2>
+
+            <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange}
+             newNumber={newNumber} handleNumberChange={handleNumberChange} />
+
+            <h2>Numbers</h2>
+
+            <Persons persons={personsToShow} />
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
+    )
 
 }
 
