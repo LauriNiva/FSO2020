c4cb956f45230d97e74459c85b4a6a5c98044e71
Lauri Niva <niva.lauri@gmail.com>
2021-03-10 07:54:46 +0200

    p2-mt: Created react app

---
+++ osa2/p2-maiden-tiedot/src/index.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import axios from "axios";
+
+const App = () => {
+  return <div>
+    Find countries
+  </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
