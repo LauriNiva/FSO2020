03b8502b75520927e77d9af379b25456aecc3f36
Lauri Niva <niva.lauri@gmail.com>
2021-05-25 10:28:05 +0300

    Part4 blogilista: step2

--- osa4/blogilista/utils/list_helpers.js
+++ osa4/blogilista/utils/list_helpers.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (!blogs.length) return 0;
+
+  const reducer = (total, blog) => total + blog.likes;
+  
+  return blogs.reduce( reducer, 0);
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 };
