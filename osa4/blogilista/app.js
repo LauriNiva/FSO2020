4795b6bf00ba0148b7e1fe4d58cfb3dd8dfbc08f
Lauri Niva <niva.lauri@gmail.com>
2021-05-27 20:54:15 +0300

    P4:blogilista 4.8 step1

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs.controller');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 app.use(cors());
 app.use(express.json());
---
+++ osa4/blogilista/tests/api.test.js
@@ -0,0 +1,27 @@
+const mongoose = require ('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog.model');
+const helper = require('./test_helpers');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('returns correct ammount of blogs', async () => {
+  const res = await api.get('/api/blogs');
+  expect(res.body).toHaveLength(helper.initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
