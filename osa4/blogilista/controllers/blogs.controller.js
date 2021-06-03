d5dbbe68a714678e55dbf84fe6532b28484ff25d
Lauri Niva <niva.lauri@gmail.com>
2021-06-03 19:08:26 +0300

    P4-blogilista: 4.19
    Authenticate user before adding new blog

--- osa4/blogilista/controllers/blogs.controller.js
+++ osa4/blogilista/controllers/blogs.controller.js
@@ -1,6 +1,15 @@
 const Blog = require('../models/blog.model');
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, _id: 1});
@@ -8,10 +17,16 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if(!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const blog = new Blog(request.body);
   if (!blog.likes) blog.likes = 0;
 
-  const user = await User.findOne({});
   blog.user = user._id;
 
   const savedBlog = await blog.save();
