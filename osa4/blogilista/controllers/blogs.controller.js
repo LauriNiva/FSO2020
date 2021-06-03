84ce3b559d81b0d128031193f0648ad23ec13338
Lauri Niva <niva.lauri@gmail.com>
2021-06-03 19:22:27 +0300

    P4-blogilista: 4.20
    Created middleware for tokenexctracting

--- osa4/blogilista/controllers/blogs.controller.js
+++ osa4/blogilista/controllers/blogs.controller.js
@@ -3,13 +3,6 @@ const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, _id: 1});
@@ -17,9 +10,8 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if(!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if(!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' });
   }
   const user = await User.findById(decodedToken.id);
