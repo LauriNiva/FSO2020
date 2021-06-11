6e2779a5d207bc02ed556f6d3d855036dc4c9d00
Lauri Niva <niva.lauri@gmail.com>
2021-06-11 19:59:22 +0300

    P5-blogilista-frontend: step7
    Painike blogin kohdalle, jolla laajennetaan näyttämään muut blogin tiedot

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: '1px solid',
+    marginBottom: 5
+  }
+
+  const [showFullInfo, setShowFullInfo] = useState(false);
+
+  const toggleView = () => {
+    setShowFullInfo(!showFullInfo);
+  }
+  
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleView}>
+        {showFullInfo ? 'Hide' : 'View'}
+      </button>
+      {showFullInfo 
+      ? <div>
+        <div>{blog.url}</div>
+        <div>Likes: {blog.likes}<button>Like</button></div>
+        <div>{blog.user.name}</div>
+        </div>
+      : ''
+      }
+    </div>
+  )
+};
 
 export default Blog
