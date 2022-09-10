97506f0b2f84c163bbe963801a1feb0b3301eb62
Lauri Niva <niva.lauri@gmail.com>
2022-09-10 16:58:51 +0300

    P5: blogilista: 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 
 describe('<Blog />', () => {
 
-  const testBlog = { title: 'Test Blog', author: 'Test Author', url: 'testurl.com' };
 
-  render(<Blog blog={testBlog} />);
+  beforeEach(() => {
+    const testUser = { name: 'Testname' };
+    const testBlog = { title: 'Test Blog', author: 'Test Author', url: 'testurl.com', likes: 3, user: testUser };
+
+    render(<Blog blog={testBlog} user={testUser} />);
+
+  });
+
+
+
 
   test('Blog renders title and author, but not url and likes', async () => {
     screen.getByText('Test Blog');
@@ -20,4 +29,18 @@ describe('<Blog />', () => {
     expect(likesContainer).toBeNull();
 
   });
+
+  test('Blog renders url and likes after the View-button is pressed', async () => {
+
+    const user = userEvent.setup();
+
+    const button = screen.getByText('View');
+
+    await user.click(button);
+    screen.debug();
+    screen.getByText('testurl.com');
+    const likes = screen.queryByText('Likes: ', { exact: false });
+    expect(likes).not.toBeNull();
+  });
+
 });
