9d4e7eb4b8914e0a6370a3681c14e01a2b206bb0
Lauri Niva <niva.lauri@gmail.com>
2022-09-10 12:05:35 +0300

    P5: notes frontend: Note tests

---
+++ osa5/P5-notes-frontend/src/components/Note.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Note from './Note';
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  };
+
+  const component = render(
+    <Note note={note} />
+  );
+
+  screen.debug();
+
+  // tapa 1
+  expect(component.container).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  );
+
+  // tapa 2
+  const element = component.getByText(
+    'Component testing is done with react-testing-library'
+  );
+  expect(element).toBeDefined();
+
+  // tapa 3
+  const div = component.container.querySelector('.note');
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  );
+
+});
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  };
+
+  const mockHandler = jest.fn();
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText('make not important');
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLenght(1);
+
+});
