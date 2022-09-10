3fe74586f7c14c365a8dbc8d58c7d42ab395cb2a
Lauri Niva <niva.lauri@gmail.com>
2022-09-10 16:21:04 +0300

    P5: blogilista: 5.13

---
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Blog from './Blog';
+import { exact } from 'prop-types';
+
+
+describe('<Blog />', () => {
+
+  const testBlog = { title: 'Test Blog', author: 'Test Author', url: 'testurl.com' };
+
+  render(<Blog blog={testBlog} />);
+
+  test('Blog renders title and author, but not url and likes', async () => {
+    screen.getByText('Test Blog');
+    screen.getByText('Test Author');
+
+    const url = screen.queryByText('testurl.com');
+    expect(url).toBeNull();
+    const likesContainer = screen.queryByText('Likes: ', { exact: false });
+    expect(likesContainer).toBeNull();
+
+  });
+});
