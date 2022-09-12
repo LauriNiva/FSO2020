316e7582b312ca562ed1cc89fc2da3c0dcbd10aa
Lauri Niva <niva.lauri@gmail.com>
2022-09-12 18:58:54 +0300

    P5: bloglista: 5.19

--- osa5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -20,11 +20,11 @@ describe('Blogg app', function() {
       cy.get('input[name="Username"]').type('niva');
       cy.get('input[name="Password"]').type('salasana');
       cy.get('button').contains('login').click();
-      
+
       cy.contains('Lauri Niva logged in');
-      
+
     });
-    
+
     it('fails with wrong credentials', function(){
       cy.get('input[name="Username"]').type('niva');
       cy.get('input[name="Password"]').type('väärä');
@@ -33,6 +33,34 @@ describe('Blogg app', function() {
       cy.get('.notification-error')
         .should('contain', 'Error: wrong username or password!')
         .should('have.css', 'color', 'rgb(255, 0, 0)');
+    });
+  });
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/login',
+        {
+          username: 'niva',
+          password: 'salasana'
+        }
+      ).then(({ body }) => {
+        window.localStorage.setItem('loggedUser', JSON.stringify(body));
+        cy.visit('http://localhost:3000');
+      });
+    });
+
+    it('A blog can be created', function() {
+      cy.contains('Create').click();
+      cy.get('input[name="title"]').type('Test Blog');
+      cy.get('input[name="author"]').type('Tester Doe');
+      cy.get('input[name="url"]').type('testblog.com');
+      cy.get('button[type="submit"]').contains('Create').click();
+
+      cy.get('div[class="blog"]').contains('Test Blog');
+      cy.get('div[class="blog"]').contains('Tester Doe');
+
+
+
     });
   });
 
