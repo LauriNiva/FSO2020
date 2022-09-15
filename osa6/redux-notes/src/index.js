481c029f8525fae5ff67b192140ded2763d2a78d
Lauri Niva <niva.lauri@gmail.com>
2022-09-15 09:29:41 +0300

    P6: redux-notes 6-3

--- osa6/redux-notes/src/index.js
+++ osa6/redux-notes/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
+import noteService from './services/notes';
 
-import noteReducer, { createNote } from './reducers/noteReducer'
-import filterReducer, { filterChange } from './reducers/filterReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
   reducer: {
@@ -14,10 +15,10 @@ const store = configureStore({
   }
 })
 
+noteService.getAll().then(notes => store.dispatch(setNotes(notes)))
+
 console.log(store.getState())
 store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 // store.dispatch({
 //   type: 'NEW_NOTE',
--- osa6/redux-notes/src/reducers/noteReducer.js
+++ osa6/redux-notes/src/reducers/noteReducer.js
@@ -1,18 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id:1
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2
-  }
-];
-
+const initialState = [];
 
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
@@ -22,12 +10,7 @@ const noteSlice = createSlice({
   initialState,
   reducers: {
     createNote(state, action) {
-      const content = action.payload;
-      state.push({
-        content,
-        imporntan: false,
-        id: generateId()
-      })
+      state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
       const id = action.payload;
@@ -38,9 +21,15 @@ const noteSlice = createSlice({
       }
       return state.map(note => 
         note.id !== id ? note : changedNote)
+    },
+    appendNote(state, action) {
+      state.push(action.payload);
+    },
+    setNotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions;
 export default noteSlice.reducer;
