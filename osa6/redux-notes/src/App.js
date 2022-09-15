702b1140332a86f32836bfd3f9189836f5057e77
Lauri Niva <niva.lauri@gmail.com>
2022-09-15 10:36:40 +0300

    P6: redux-notes 6-4

--- osa6/redux-notes/src/App.js
+++ osa6/redux-notes/src/App.js
@@ -3,7 +3,7 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 import noteService from './services/notes'
-import { setNotes } from './reducers/noteReducer'
+import { initializeNotes, setNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(()=> {
-    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+    dispatch(initializeNotes())
   }, [dispatch])
 
 
--- osa6/redux-notes/src/index.js
+++ osa6/redux-notes/src/index.js
@@ -1,42 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-import noteService from './services/notes';
-
-import noteReducer, { setNotes } from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-noteService.getAll().then(notes => store.dispatch(setNotes(notes)))
-
-console.log(store.getState())
-store.subscribe(() => console.log(store.getState()))
-
-// store.dispatch({
-//   type: 'NEW_NOTE',
-//   data: {
-//     content: 'the app state is in redux store',
-//     important: true,
-//     id: 1
-//   }
-// })
-
-// store.dispatch({
-//   type: 'NEW_NOTE',
-//   data: {
-//     content: 'state changes are made with actions',
-//     important: false,
-//     id: 2
-//   }
-// })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
