88e00217e8b26a6c227a1172be9d6c3475297752
Lauri Niva <niva.lauri@gmail.com>
2022-11-23 11:24:10 +0200

    P8: apollo-test-backend: 8-5

--- osa8/apollo-test/app.js
+++ osa8/apollo-test/app.js
@@ -1,8 +1,17 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server');
+const {
+  ApolloServer,
+  UserInputError,
+  AuthenticationError,
+  gql,
+} = require('apollo-server');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Person = require('./models/person');
+const User = require('./models/user');
 
 const MONGODB_URI = 'mongodb+srv://databaseurlhere';
+
+const JWT_SECRET = 'NEED_HERE_A_SECRET_KEY';
 
 console.log('connecting to', MONGODB_URI);
 
@@ -56,10 +65,21 @@ const typeDefs = gql`
     NO
   }
 
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     personCount: Int!
     allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
+    me: User
   }
 
   type Mutation {
@@ -69,7 +89,14 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+
+    addAsFriend(name: String!): User
+
     editNumber(name: String!, phone: String!): Person
+
+    createUser(username: String!): User
+
+    login(username: String!, password: String!): Token
   }
 `;
 
@@ -83,6 +110,9 @@ const resolvers = {
       return Person.find({ phone: { $exists: args.phone === 'YES' } });
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
   },
   Person: {
     address: (root) => {
@@ -93,16 +123,43 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args });
+      const currentUser = context.currentUser;
+
+      if (!currentUser) {
+        throw new AuthenticationError('not authenticated');
+      }
+
       try {
         await person.save();
+        currentUser.friends = currentUser.friends.concat(person);
+        await currentUser.save();
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
         });
       }
       return person;
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      const nonFriendAlready = (person) =>
+        !currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString());
+
+      if (!currentUser) {
+        throw new AuthenticationError('not authenticated');
+      }
+
+      const person = await Person.findOne({ name: args.name });
+      if (nonFriendAlready(person)) {
+        currentUser.friends = currentUser.friends.concat(person);
+      }
+
+      await currentUser.save();
+
+      return currentUser;
     },
     editNumber: async (root, args) => {
       const person = await Person.findOne({ name: args.name });
@@ -116,12 +173,45 @@ const resolvers = {
       }
       return person;
     },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      return user.save().catch((error) => {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      });
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError('wrong credentials');
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id).populate(
+        'friends'
+      );
+      return { currentUser };
+    }
+  },
 });
 
 server.listen().then(({ url }) => {
